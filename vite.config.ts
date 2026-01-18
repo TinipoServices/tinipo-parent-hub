a9d2eba2e989910dd5bab5dcde4d12d2e3b40c6d
eggozjk <jitendra.gupta@eggoz.in>
2026-01-18 23:32:53 +0530

    added

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
+// CHANGE ONLY THIS PART 👇
 export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/tinipo-parent-hub/" : "/",
+
   server: {
     host: "::",
     port: 8080,
@@ -12,10 +14,16 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),
+  ].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
 }));
+
